377dc32b2bacebff29623f2b135cb303e986538e
GOD-MRX <81347639+GOD-MRX@users.noreply.github.com>
2021-03-30 11:21:34 -0600

    Create skgen.sh

---
+++ skgen.sh
@@ -0,0 +1,8 @@
+clear
+echo " _           __"
+echo "|_) o __  _ /__ _ __"
+echo "|_) | | |_> \_|(/_| |"
+echo "=========="
+echo "Herramienta generadora de cc="
+echo "=========="
+python .skgen.py
